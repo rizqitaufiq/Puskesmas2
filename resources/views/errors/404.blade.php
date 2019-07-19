cd840903a71ccde5e074b1459936c2b50801f4a7
Rizqi Taufiq F A <akatsukirizqi@gmail.com>
2019-07-19 23:18:30 +0700

    create link not found and delete file
    membuat tampilan halaman tidak ditemukan dan menghapus beberapa file tidak ditemukan

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,13 +9,12 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>Ela - Bootstrap Admin Dashboard Template</title>
+     <link rel="icon" type="{{ asset('../../admin/image/png')}}" sizes="16x16" href="{{ asset('../../admin/images/favicon.png')}}">
+    <title>Form Evaluasi Program | Not Found</title>
     <!-- Bootstrap Core CSS -->
-    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="css/helper.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="{{ asset('../../admin/css/lib/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('../../admin/css/helper.css')}}" rel="stylesheet">
+    <link href="{{ asset('../../admin/css/style.css')}}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
     <!--[if lt IE 9]>
@@ -34,27 +33,27 @@
     <div class="error-page" id="wrapper">
         <div class="error-box">
             <div class="error-body text-center">
-                <h1>403</h1>
-                <h3 class="text-uppercase">Forbiddon Error! </h3>
-                <p class="text-muted m-t-30 m-b-30">Please try after some time</p>
-                <a class="btn btn-info btn-rounded waves-effect waves-light m-b-40" href="index.html">Back to home</a> </div>
-            <footer class="footer text-center">&copy; 2018 Zebramin.</footer>
+                <h1>404</h1>
+                <h3 class="text-uppercase">Halaman Tidak Ditemukan</h3>
+                <p class="text-muted m-t-30 m-b-30"></p>
+                <a class="btn btn-info btn-rounded waves-effect waves-light m-b-40" href="{{route('home')}}">Kembali</a> </div>
+            <footer class="footer text-center">&copy; 2019 rereasdev</footer>
         </div>
     </div>
     <!-- End Wrapper -->
     <!-- All Jquery -->
-    <script src="js/lib/jquery/jquery.min.js"></script>
+    <script src="{{ asset('../../admin/js/lib/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap tether Core JavaScript -->
-    <script src="js/lib/bootstrap/js/popper.min.js"></script>
-    <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ asset('../../admin/js/lib/bootstrap/js/popper.min.js')}}"></script>
+    <script src="{{ asset('../../admin/js/lib/bootstrap/js/bootstrap.min.js')}}"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="js/jquery.slimscroll.js"></script>
+    <script src="{{ asset('../../admin/js/jquery.slimscroll.js')}}"></script>
     <!--Menu sidebar -->
-    <script src="js/sidebarmenu.js"></script>
+    <script src="{{ asset('../../admin/js/sidebarmenu.js')}}"></script>
     <!--stickey kit -->
-    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="{{ asset('../../admin/js/lib/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
     <!--Custom JavaScript -->
-    <script src="js/scripts.js"></script>
+    <script src="{{ asset('../../admin/js/scripts.js')}}"></script>
 
 </body>
 
